ee2d42c79b69d51d9f2d8d049c8cf90921e7dfbb
Aidata777 <aidata.core001@gmail.com>
2025-05-13 03:30:25 -0600

    Update render_service.py

--- render_service.py
+++ render_service.py
@@ -5,16 +5,31 @@ from io import BytesIO
 
 app = FastAPI()
 
-def resize_image(img: Image.Image, target_width: int, target_height: int) -> Image.Image:
-    return img.resize((target_width, target_height), Image.LANCZOS)
+def smart_crop(image: Image.Image, target_width: int, target_height: int) -> Image.Image:
+    img_width, img_height = image.size
+    target_ratio = target_width / target_height
+    img_ratio = img_width / img_height
+
+    # Decide crop strategy
+    if img_ratio > target_ratio:
+        # Crop width
+        new_width = int(target_ratio * img_height)
+        offset = (img_width - new_width) // 2
+        cropped = image.crop((offset, 0, offset + new_width, img_height))
+    else:
+        # Crop height
+        new_height = int(img_width / target_ratio)
+        offset = (img_height - new_height) // 2
+        cropped = image.crop((0, offset, img_width, offset + new_height))
+
+    return cropped.resize((target_width, target_height), Image.LANCZOS)
 
 @app.post("/generate/horizontal")
 async def generate_horizontal(file: UploadFile = File(...), text: str = Form(...)):
     img = Image.open(BytesIO(await file.read()))
-    resized = resize_image(img, 1280, 720)
+    resized = smart_crop(img, 1280, 720)
 
     output = BytesIO()
     resized.save(output, format="PNG")
     output.seek(0)
-
     return StreamingResponse(output, media_type="image/png")
