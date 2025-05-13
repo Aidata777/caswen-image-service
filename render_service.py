225d85985b9202f53e2df9d294c63068d59ab7bf
Aidata777 <aidata.core001@gmail.com>
2025-05-13 01:48:25 -0600

    Update render_service.py

--- render_service.py
+++ render_service.py
@@ -1,56 +1,38 @@
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import StreamingResponse
+import os
+import uvicorn
+from fastapi import FastAPI, UploadFile, File
 from PIL import Image
-import io
+from io import BytesIO
 
 app = FastAPI()
-
-def resize_and_crop(img: Image.Image, target_width: int, target_height: int) -> Image.Image:
-    original_ratio = img.width / img.height
-    target_ratio = target_width / target_height
-
-    if original_ratio > target_ratio:
-        new_height = target_height
-        new_width = int(new_height * original_ratio)
-    else:
-        new_width = target_width
-        new_height = int(new_width / original_ratio)
-
-    img = img.resize((new_width, new_height), Image.LANCZOS)
-
-    left = (new_width - target_width) / 2
-    top = (new_height - target_height) / 2
-    right = (new_width + target_width) / 2
-    bottom = (new_height + target_height) / 2
-
-    return img.crop((left, top, right, bottom))
 
 @app.post("/generate/horizontal")
 async def generate_horizontal(file: UploadFile = File(...)):
     contents = await file.read()
-    image = Image.open(io.BytesIO(contents)).convert("RGB")
-    processed_image = resize_and_crop(image, 1200, 675)
-    buffer = io.BytesIO()
-    processed_image.save(buffer, format="JPEG")
-    buffer.seek(0)
-    return StreamingResponse(buffer, media_type="image/jpeg")
+    image = Image.open(BytesIO(contents))
 
-@app.post("/generate/vertical")
-async def generate_vertical(file: UploadFile = File(...)):
-    contents = await file.read()
-    image = Image.open(io.BytesIO(contents)).convert("RGB")
-    processed_image = resize_and_crop(image, 1080, 1350)
-    buffer = io.BytesIO()
-    processed_image.save(buffer, format="JPEG")
-    buffer.seek(0)
-    return StreamingResponse(buffer, media_type="image/jpeg")
+    width, height = image.size
+    new_width = int(width)
+    new_height = int(width * 9 / 16)
 
-@app.post("/generate/square")
-async def generate_square(file: UploadFile = File(...)):
-    contents = await file.read()
-    image = Image.open(io.BytesIO(contents)).convert("RGB")
-    processed_image = resize_and_crop(image, 1080, 1080)
-    buffer = io.BytesIO()
-    processed_image.save(buffer, format="JPEG")
-    buffer.seek(0)
-    return StreamingResponse(buffer, media_type="image/jpeg")
+    if new_height > height:
+        new_height = height
+        new_width = int(height * 16 / 9)
+
+    left = (width - new_width) / 2
+    top = (height - new_height) / 2
+    right = left + new_width
+    bottom = top + new_height
+
+    cropped = image.crop((left, top, right, bottom))
+
+    output = BytesIO()
+    cropped.save(output, format="PNG")
+    output.seek(0)
+
+    return StreamingResponse(output, media_type="image/png")
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("render_service:app", host="0.0.0.0", port=port, reload=False)
