2dd80f34d6935a6b7feed58c3aecbf51db8969d2
Aidata777 <aidata.core001@gmail.com>
2025-05-14 01:03:32 -0600

    Update render_service.py

--- render_service.py
+++ render_service.py
@@ -10,15 +10,28 @@ os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 def smart_crop_resize(image: Image.Image, width: int, height: int) -> Image.Image:
     cropper = smartcrop.SmartCrop()
-    result = cropper.crop(image, width, height)
+    
+    # Forzamos una relación de aspecto deseada
+    target_ratio = width / height
+    base_crop_width = image.width
+    base_crop_height = int(base_crop_width / target_ratio)
+
+    if base_crop_height > image.height:
+        base_crop_height = image.height
+        base_crop_width = int(base_crop_height * target_ratio)
+
+    result = cropper.crop(image, base_crop_width, base_crop_height)
+
     box = (
         result['top_crop']['x'],
         result['top_crop']['y'],
         result['top_crop']['x'] + result['top_crop']['width'],
         result['top_crop']['y'] + result['top_crop']['height'],
     )
+
     cropped = image.crop(box)
-    return cropped.resize((width, height), Image.LANCZOS)
+    resized = cropped.resize((width, height), Image.LANCZOS)
+    return resized
 
 @app.post("/instagram")
 async def render_instagram(file: UploadFile = File(...)):
